016541bd177cb24402f725e2abfda8bad9884879
Brad Slagle <btslagle@gmail.com>
2022-03-29 17:13:37 -0500

    Attempt exercise 4

---
+++ exercises/4/index.js
@@ -0,0 +1,15 @@
+const ul = document.querySelector("ul");
+const li = document.querySelector("li");
+
+li.classList.add("corgi")
+li.classList.remove("dog");
+
+
+/*li.classList.forEach((class) => {
+
+
+
+/*li.classList.map((classList) => {
+     
+    return li.classList;
+})*/

d48886b649152c102a8d8d9bf8ef99ba7b20d667
Brad Slagle <btslagle@gmail.com>
2022-03-30 11:49:13 -0500

    Answer 12

---
+++ exercises/12/index.js
@@ -0,0 +1,6 @@
+const div = document.querySelector("div");
+const ul = document.createElement("ul");
+div.append(ul)
+const li = document.createElement("li");
+li.textContent = "Kiki"
+ul.append(li);

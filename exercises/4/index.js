556e64492ccf099115b5a624116840ad80334941
Brad Slagle <btslagle@gmail.com>
2022-03-30 09:54:44 -0500

    Update four

--- exercises/4/index.js
+++ exercises/4/index.js
@@ -1,15 +1,3 @@
-const ul = document.querySelector("ul");
-const li = document.querySelector("li");
+const li = document.querySelectorAll("li");
+li.forEach(li => li.classList = "corgi");
 
-li.classList.add("corgi")
-li.classList.remove("dog");
-
-
-/*li.classList.forEach((class) => {
-
-
-
-/*li.classList.map((classList) => {
-     
-    return li.classList;
-})*/
